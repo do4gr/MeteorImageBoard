e1548d5b1532ea2c965b4be146ac92f0addfd0b6
ViktorAttol <j.kolesnyk@gmx.de>
2017-06-02 11:01:14 +0200

    my Posts in profile woerks

--- meteor-apollo-graphql-login-merged/imports/ui/containers/profileLists/MyPosts.js
+++ meteor-apollo-graphql-login-merged/imports/ui/containers/profileLists/MyPosts.js
@@ -1,15 +1,17 @@
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
-import ListPage from '../../components/ListPage';
+import ListPage from '../../components/profile/ProfileListPage';
 
 
 //TODO
 const MyPostsQuery = gql`query {
-  allPosts(orderBy: createdAt_DESC) {
-    id
-	postedFile { url }
-    description
-  }
+    user{
+        posts(orderBy: createdAt_DESC) {
+            id
+        	postedFile { url }
+            description
+        }
+    }
 }`
 
 export default graphql(MyPostsQuery)(ListPage);
