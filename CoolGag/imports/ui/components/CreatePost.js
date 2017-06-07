dfdc13a486d85e320f10bab45fa74cf957a31143
Richard Wepner <wepner@campus.tu-berlin.de>
2017-06-07 13:10:43 +0200

    Generate image
    properly generate the image to upload from the selecte image and the
    entered text

--- CoolGag/imports/ui/components/CreatePost.js
+++ CoolGag/imports/ui/components/CreatePost.js
@@ -5,6 +5,7 @@ import gql from 'graphql-tag';
 import {Button} from 'reactstrap';
 import ContentEditable from 'react-contenteditable';
 import ReactDOM from 'react-dom';
+import html2canvas from 'html2canvas';
 
 class CreatePost extends React.Component {
 
@@ -20,6 +21,7 @@ class CreatePost extends React.Component {
 		imageUrl: '',
 		imageSize: {width: 0, height: 0},
 		isSubmitting: false,
+		isRendering: false,
 		file: null,
 		postedFileId: '',
 		isDraggingFile: false,
@@ -34,7 +36,20 @@ class CreatePost extends React.Component {
 	}
 	
 	static fontSizePercentage = 0.09;
-	static textStyle = {};
+	static textStyle = {
+		'text-align': 'center',
+		'font-family': 'impact',
+		'color': 'white',
+		'position': 'absolute',
+		'width': '100%'
+		//'text-shadow': '-0.0625em -0.0625em 0 #000, 0.0625em -0.0625em 0 #000,-0.0625em  0.0625em 0 #000,0.0625em  0.0625em 0 #000,-0.0625em  0em 0 #000,0.0625em  0em 0 #000, 0em 0.0625em 0 #000, 0em -0.0625em 0 #000'
+	};
+	static upperTextStyle = {
+		'top': '0'
+	};
+	static lowerTextStyle = {
+		'bottom': '.25em' // not 0, since thas caused the text to have no gap towards the bottom at all
+	};
 
 	isSubmittable() {
 		return this.state.description && this.state.file && !this.state.isSubmitting;
@@ -202,13 +217,14 @@ class CreatePost extends React.Component {
 							}
 						</div>
 					}
-					<button type="submit" disabled={(this.isSubmittable() ? "" : "disabled")} className={'pa3 bn ttu pointer' + (this.isSubmittable() ? " bg-black-10 dim" : " black-30 bg-black-05 disabled")}>{this.state.isSubmitting ? 'Submitting ...' : 'Post'}</button>
+					<button type="submit" disabled={(this.isSubmittable() ? "" : "disabled")} className={'pa3 bn ttu pointer' + (this.isSubmittable() ? " bg-black-10 dim" : " black-30 bg-black-05 disabled")}>
+						{this.state.isSubmitting ? (this.state.isRendering ? 'Rendering...' : 'Submitting ...') : 'Post'}
+					</button>
+					<canvas id="testCanvas" style={{'width': '0px', 'height': '0px', 'position': 'absolute', 'margin': '0', 'padding': '0'}}></canvas>
 				</form>
 			</div>
 		)
 	}
-				//<button type="button" onClick={this.generateImage.bind(this)}>Test Image Generation</button>
-				//<canvas id="generationTest" width="400" height="300" />
 	
 	recalcImageFontSize(element) {
 		if(!element) {
@@ -224,31 +240,44 @@ class CreatePost extends React.Component {
 		event.preventDefault();
 		this.setState({'isSubmitting': true});
 		
-		let data = new FormData();
-		data.append('data', this.state.file);
-
-		fetch('https://api.graph.cool/file/v1/cj2ryvxmbt4qw0160y6qhdgdl', {
-			body: data,
-			method: 'POST'
-		}).then((response) => {
-			response.json().then(result => {
-				//self.setState({imageUrl: result.url});
-				this.setState({postedFileId: result.id});
-				this.setState({userId: this.props.data.user.id});
-				var {description, category, postedFileId, userId} = this.state
-				if(category == "") {
-					category = null;
-				}
-				this.props.mutate({variables: {description, postedFileId, category, userId}})
-					.then(() => {
-						this.props.router.replace('/')
-					});
+		var continueUpload = () => {
+			fetch('https://api.graph.cool/file/v1/cj2ryvxmbt4qw0160y6qhdgdl', {
+				body: data,
+				method: 'POST'
+			}).then((response) => {
+				response.json().then(result => {
+					//self.setState({imageUrl: result.url});
+					this.setState({postedFileId: result.id});
+					this.setState({userId: this.props.data.user.id});
+					var {description, category, postedFileId, userId} = this.state
+					if(category == "") {
+						category = null;
+					}
+					this.props.mutate({variables: {description, postedFileId, category, userId}})
+						.then(() => {
+							this.props.router.replace('/')
+						});
+				});
+			}).catch((exception) => {
+				// TODO: handle upload error
+				console.log('error uploading the file!');
+				this.setState({'isSubmitting': false});
 			});
-		}).catch((exception) => {
-			// TODO: handle upload error
-			console.log('error uploading the file!');
-			this.setState({'isSubmitting': false});
-		});
+		};
+		
+		let data = new FormData();
+		if(this.state.isTextEntered) {
+			this.generateImage({
+				callback: (dataUrl) => {
+					var blob = this.dataURItoBlob(dataUrl);
+					data.append('data', blob);
+					continueUpload();
+				}
+			});
+		} else {
+			data.append('data', this.state.file);
+			continueUpload();
+		}
 		
 		return false;
 	}
@@ -314,48 +343,152 @@ class CreatePost extends React.Component {
 			}
 		}
 	}
+	styleObjectToString() {
+		result = "";
+		for(var i = 0; i < arguments.length; i++) {
+			var arg = arguments[i];
+			for(var key in arg) {
+				result += key + ':' + arg[key] + ';';
+			}
+		}
+		return result;
+	}
 	
 	// Image from DOM: https://developer.mozilla.org/de/docs/Web/HTML/Canvas/Drawing_DOM_objects_into_a_canvas
-	generateImage() {
-		//var canvas = document.createElement('canvas');
-		var canvas = document.getElementById('generationTest');
-		var ctx = canvas.getContext('2d');
-		
-		canvas.width = this.state.imageSize.width;
-		canvas.height = this.state.imageSize.height;
-		canvas.style = 'width: ' + canvas.width + 'px; height: ' + canvas.height + 'px';
-		var fontSize = this.state.imageSize.width * CreatePost.fontSizePercentage;
-		
-		var data =
-			'<svg xmlns="http://www.w3.org/2000/svg" width="' + canvas.width + '" height="' + canvas.height + '">' +
-				'<foreignObject width="100%" height="100%">' +
-					'<div xmlns="http://www.w3.org/1999/xhtml" style="font-size:' + fontSize + 'px">' +
-						'<img src="' + this.state.imageUrl + '" />' +
-						'<em>I</em> like ' + 
-						'<span style="color:white; text-shadow:0 0 2px blue;">' +
-						'cheese</span>' +
-					'</div>' +
-				'</foreignObject>' +
-			'</svg>';
-
-		var DOMURL = window.URL || window.webkitURL || window;
-		
+	generateImage(options) {
+		this.setState({'isRendering': true});
 		var img = new Image();
-		var svg = new Blob([data], {type: 'image/svg+xml'});
-		var url = DOMURL.createObjectURL(svg);
 		
-		img.onload = function () {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			ctx.drawImage(img, 0, 0);
-			DOMURL.revokeObjectURL(url);
+		img.onload = () => {
+			var width = this.state.imageSize.width;
+			var height = this.state.imageSize.height;
+			var canvas = document.createElement('canvas');
+			//var canvas = document.getElementById('generationTest');
+			var ctx = canvas.getContext('2d');
+			canvas.width = width;
+			canvas.height = height;
+			canvas.style = 'width: ' + 400 + 'px;';
+			var fontSize = width * CreatePost.fontSizePercentage;
+			
+			var blackUpperText;
+			var whiteUpperText;
+			var blackLowerText;
+			var whiteLowerText;
+			
+			this.drawText({
+				html: this.state.upperImageText,
+				fontSize: fontSize,
+				width: width,
+				height: height,
+				color: 'black'
+			}).then((result)=>{
+				blackUpperText = result;
+				this.drawText({
+					html: this.state.upperImageText,
+					fontSize: fontSize,
+					width: width,
+					height: height,
+					color: 'white'
+				}).then((result)=>{
+					whiteUpperText = result;
+					this.drawText({
+						html: this.state.lowerImageText,
+						fontSize: fontSize,
+						width: width,
+						height: height,
+						color: 'black'
+					}).then((result)=>{
+						blackLowerText = result;
+						this.drawText({
+							html: this.state.lowerImageText,
+							fontSize: fontSize,
+							width: width,
+							height: height,
+							color: 'white'
+						}).then((result)=>{
+							whiteLowerText = result;
+							
+							//ctx.drawImage(blackUpperText, -5, fontSize / 5-5);
+							ctx.clearRect(0, 0, canvas.width, canvas.height);
+							ctx.drawImage(img, 0, 0);
+							var offset = fontSize / 16;
+							ctx.drawImage(blackUpperText, -offset, fontSize / 16 - offset);
+							ctx.drawImage(blackUpperText, -offset, fontSize / 16);
+							ctx.drawImage(blackUpperText, -offset, fontSize / 16 + offset);
+							ctx.drawImage(blackUpperText, 0, fontSize / 16 - offset);
+							ctx.drawImage(blackUpperText, 0, fontSize / 16 + offset);
+							ctx.drawImage(blackUpperText, offset, fontSize / 16 - offset);
+							ctx.drawImage(blackUpperText, offset, fontSize / 16);
+							ctx.drawImage(blackUpperText, offset, fontSize / 16 + offset);
+							ctx.drawImage(whiteUpperText, 0, fontSize / 16);
+							
+							ctx.drawImage(blackLowerText, -offset, height - blackLowerText.height - offset);
+							ctx.drawImage(blackLowerText, -offset, height - blackLowerText.height);
+							ctx.drawImage(blackLowerText, -offset, height - blackLowerText.height + offset);
+							ctx.drawImage(blackLowerText, 0, height - blackLowerText.height - offset);
+							ctx.drawImage(blackLowerText, 0, height - blackLowerText.height + offset);
+							ctx.drawImage(blackLowerText, offset, height - blackLowerText.height - offset);
+							ctx.drawImage(blackLowerText, offset, height - blackLowerText.height);
+							ctx.drawImage(blackLowerText, offset, height - blackLowerText.height + offset);
+							ctx.drawImage(whiteLowerText, 0, height - whiteLowerText.height);
+							
+							var dataUrl = canvas.toDataURL('image/jpeg', 0.85);
+							
+							this.setState({'isRendering': false});
+							if(options && typeof options.callback == 'function') {
+								options.callback(dataUrl);
+							}
+						});
+					});
+				});
+			});
 		}
 		
-		img.src = url;
+		img.src = this.state.imageUrl;
+	}
+	
+	drawText(options) {
+		var color = options && options.color ? options.color : 'black';
+		var html = options && options.html ? options.html : '';
+		var width = options && options.width ? options.width : 400;
+		var height = options && options.height ? options.height : 400;
+		var fontSize = options && options.fontSize ? options.fontSize : 36;
+		var callback = null;
+		
+		var frame = document.createElement('iframe');
+		frame.setAttribute('style', 'position:absolute;top:0;left:0;width:0;height:0;');
+		frame.setAttribute('frameBorder','0');
+		frame.onload = () => {
+			var divContainer = $('<div style="width:' + width +'px; height: ' + height +'px;"></div>')[0];
+			var upperStyle = this.styleObjectToString(CreatePost.textStyle, {'font-size': fontSize + 'px', width: width + 'px;', color: color, position: 'relative'});
+			var lowerStyle = this.styleObjectToString(CreatePost.textStyle, {'font-size': fontSize + 'px', width: width + 'px;', color: color, position: 'relative'});
+			var div = $('<div style="' + upperStyle + '">' + html + '</div>')[0];
+			divContainer.appendChild(div);
+			frame.contentDocument.body.appendChild(divContainer);
+			html2canvas(div, {
+				width: width,
+				onrendered: (canvas) => {
+					frame.contentDocument.body.removeChild(divContainer);
+					document.body.removeChild(frame);
+					if(callback) {
+						callback(canvas);
+					}
+				}
+			});
+		};
+		
+		document.body.appendChild(frame);
+		
+		return {
+			then: (c)=> {
+				callback = c;
+			}
+		};
 	}
 	
 	// used to create submittable content from an image url
 	// see: https://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
-	dataURItoBlob(dataURI) {
+	dataURItoBlob(dataURI, options) {
 		// convert base64/URLEncoded data component to raw binary data held in a string
 		var byteString;
 		var parts = dataURI.split(',');
@@ -372,7 +505,7 @@ class CreatePost extends React.Component {
 		for (var i = 0; i < byteString.length; i++) {
 			ia[i] = byteString.charCodeAt(i);
 		}
-
+		console.log('type:', mimeString);
 		return new Blob([ia], {type:mimeString});
 	}
 }
