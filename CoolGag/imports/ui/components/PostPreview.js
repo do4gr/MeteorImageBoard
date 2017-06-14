cea45b64d75c9b7ab8beb772d8ac072594824025
Johanna D <johanna.dietzz@gmail.com>
2017-06-14 10:58:46 +0200

    width of main feed and detail post to 80percent

--- CoolGag/imports/ui/components/PostPreview.js
+++ CoolGag/imports/ui/components/PostPreview.js
@@ -16,60 +16,61 @@ class PostPreview extends React.Component {
 	
 	render () {
 		return (
-			<div className='list-container'>
-				<div className='pb3'>
-					{TagUtils.splitByTagsAndRefs(this.props.post.description).map((element, index)=>{
-						if (element.type == 'tag') {
-							return (
-								<Link key={index} to={`/tag/${element.text}`}>#{element.text}</Link>
-							);
-						} else if(element.type == 'ref') {
-							return (<a href="javascript:void();" key={index}>@{element.text}</a>);
-						} else {
-							return element.text;
-							//return (<span key={index}>{element.text}</span>)
-						}
-					})}
-					&nbsp;
-				</div>
-				<div className="post-img">
-					<Link to={`/view/${this.props.post.id}`}>
-						<img src={this.props.post.postedFile.url} className='w-100' />
-					</Link>
-				</div>
-				{ this.props.post.category &&
-					<div className='pt3'>
-						{this.props.post.category}&nbsp;
+			<div>
+				<div className='list-container'>
+					<div className='pb3'>
+						{TagUtils.splitByTagsAndRefs(this.props.post.description).map((element, index)=>{
+							if (element.type == 'tag') {
+								return (
+									<Link key={index} to={`/tag/${element.text}`}>#{element.text}</Link>
+								);
+							} else if(element.type == 'ref') {
+								return (<a href="javascript:void();" key={index}>@{element.text}</a>);
+							} else {
+								return element.text;
+								//return (<span key={index}>{element.text}</span>)
+							}
+						})}
+						&nbsp;
 					</div>
-				}
-			
+					<div className="post-img">
+						<Link to={`/view/${this.props.post.id}`}>
+							<img src={this.props.post.postedFile.url} className='w-100' />
+						</Link>
+					</div>
+					{ this.props.post.category &&
+						<div className='pt3'>
+							{this.props.post.category}&nbsp;
+						</div>
+					}
+				
 
-			<span className='author-tag'>
-	           Author: {this.props.post.user ? this.props.post.user.name: "unknown user"}&nbsp;
-	        </span> 	
-			<div className="comments-points">
-		        <span>
-		         	points: {this.props.post.upvotes ? this.props.post.upvotes: "0"}&nbsp;
-		        </span>
-		        <span> | </span>
-		        <span>
-		         	comments: {this.props.post.upvotes ? this.props.post.upvotes: "0"}&nbsp;
-		        </span>
-         	</div>
-			<span>
-          		<Button className="upvote-btn"  onClick=""><span className="glyphicon glyphicon-thumbs-up"></span>UP</Button>{' '}
-        	</span>
-        	<span>
-         	 	<Button className="downvote-btn"  onClick=""><span className="glyphicon glyphicon-thumbs-down"></span>DOWN</Button>{' '}
-        	</span>
-        	<span>
-        		<Link to={`/view/${this.props.post.id}`}>
-						<Button className="comment-btn"  onClick=""><span className="glyphicon glyphicon-thumbs-down"></span>COMMENT</Button>
-				</Link>	
-        	</span>
-        	<hr/>
+				<span className='author-tag'>
+		           Author: {this.props.post.user ? this.props.post.user.name: "unknown user"}&nbsp;
+		        </span> 	
+				<div className="comments-points">
+			        <span>
+			         	points: {this.props.post.upvotes ? this.props.post.upvotes: "0"}&nbsp;
+			        </span>
+			        <span> | </span>
+			        <span>
+			         	comments: {this.props.post.upvotes ? this.props.post.upvotes: "0"}&nbsp;
+			        </span>
+	         	</div>
+				<span>
+	          		<Button className="upvote-btn"  onClick=""><span className="glyphicon glyphicon-thumbs-up"></span>UP</Button>{' '}
+	        	</span>
+	        	<span>
+	         	 	<Button className="downvote-btn"  onClick=""><span className="glyphicon glyphicon-thumbs-down"></span>DOWN</Button>{' '}
+	        	</span>
+	        	<span>
+	        		<Link to={`/view/${this.props.post.id}`}>
+							<Button className="comment-btn"  onClick=""><span className="glyphicon glyphicon-thumbs-down"></span>COMMENT</Button>
+					</Link>	
+	        	</span>
+			</div>
+			<hr/>
 		</div>
-		
 		)
 	}
 }
