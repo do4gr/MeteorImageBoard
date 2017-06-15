82eca1a09d4083599500b74fe27cdf9b963b433e
Johanna D <johanna.dietzz@gmail.com>
2017-06-16 01:59:19 +0200

    MyGroup listing a bit more designed but no functionality here

---
+++ CoolGag/imports/ui/components/profile/MyGroups.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import PostPreview from '../PostPreview'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+import PropTypes from 'prop-types'
+import { Button, Label} from 'reactstrap'
+
+export default class MyGroups extends React.Component {
+
+  static propTypes = {
+    data: PropTypes.object,
+  }
+
+  render () {
+    console.log(this.props)
+    // if (this.props.data.loading) {
+    //   return (<div>Loading</div>)
+    // }
+
+    return (
+      <div className='group-list-container'>
+
+        <div className="group-list-item-wrapper">
+        {//link to the special group site with showing Members, inviting Members, create Group, GroupPosts
+        }
+         
+          <div className="group-list-group-title">{this.props.group.name}</div>
+         
+         <div className="group-list-btn-plusMembers">
+          <Button color="secondary">Add Members</Button>
+         </div>
+         <div className="group-list-btn-leaveGroup">
+          <Button color="danger">Leave Group</Button>
+         </div>
+         
+       {/*
+          <div>
+            <b>Members</b>
+              <div>
+                {this.props.group.users.map((groupUser) =>
+                  <div> {groupUser.name},  </div>
+                )} 
+              </div>
+          </div>
+          */}
+        </div>
+      </div>
+    )
+  }
+}
+
