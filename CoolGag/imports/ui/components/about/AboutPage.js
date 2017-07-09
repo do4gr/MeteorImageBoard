10cbbee0bbc973983dc5992d8c951b885b862961
s.kotte <s.kotte@web.de>
2017-07-09 11:23:28 +0200

    updated disclaimer in about page

--- CoolGag/imports/ui/components/about/AboutPage.js
+++ CoolGag/imports/ui/components/about/AboutPage.js
@@ -22,11 +22,9 @@ class AboutPage extends React.Component {
 		  				</Jumbotron>
 		  			</Col>
 		  		</Row>
-
 		  		<Row>
 		  			<Col sm="12" md={{ size: 8, offset: 2 }} lg={{ size: 8, offset: 2 }} xl={{ size: 8, offset: 2 }} ><h2>Our Team</h2></Col>
 		  		</Row>
-
 		  		<Row>
 		  			<Col sm="12" md={{ size: 8, offset: 2 }} lg={{ size: 8, offset: 2 }} xl={{ size: 8, offset: 2 }}>
 				  		{this.props.data.allContributors && this.props.data.allContributors.map((contributor) =>
@@ -34,10 +32,17 @@ class AboutPage extends React.Component {
 		              	}
 	              	</Col>
 	            </Row>
-
 		  		<Row>
 		  			<Col sm="12" md={{ size: 8, offset: 2 }} lg={{ size: 8, offset: 2 }} xl={{ size: 8, offset: 2 }} ><h2>Disclaimer</h2></Col>
 		  		</Row>
+		  		<Row>
+		  			<Col sm="12" md={{ size: 8, offset: 2 }} lg={{ size: 8, offset: 2 }} xl={{ size: 8, offset: 2 }} >
+		  				<p>	This Website is provided by the contributors stated above and was developed as a student project for Technische Universität Berlin. This product is supplied "AS IS" without any warranties and support.
+							We assume no responsibility or liability for the use of the software, conveys no license or title under any patent, copyright, or mask work right to the product.
+							We reserves the right to make changes in the software without notification. We also make no representation or warranty that such application will be suitable for the specified use without further testing or modification.</p>
+		  			</Col>
+		  		</Row>
+
 		  	</div>
 		)
   	}
